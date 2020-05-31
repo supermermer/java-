27fbc41229d5f0c045520299d8f53dbc076a5169
supermermer <252587809@qq.com>
2020-05-31 16:28:53 +0800

    text

---
+++ src/求和/求和.java
@@ -0,0 +1,17 @@
+package 求和;
+
+public class 求和 {
+
+	public static void main(String[] args) {
+		// TODO 自动生成的方法存根
+		int i=1,sum=0;
+		
+		do {
+			sum+=i;
+			i++;
+		}
+		while(i!=100);
+		System.out.println(sum);
+	}
+
+}
